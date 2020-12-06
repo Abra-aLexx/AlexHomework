25ecf8a2f62e5b7964e4f4afa77818546aead5bd
AlexAbra <alexgithub@gmail.com>
2020-12-06 17:07:23 +0300

    homework three

---
+++ homework_three/src/main/java/com/abra/homework_three/MainActivity.java
@@ -0,0 +1,14 @@
+package com.abra.homework_three;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.task_three_with_constraint_layout);
+    }
+}
