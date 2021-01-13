7f3aa173609edad4bc3cd1cd45cee49026c5c930
AlexAbra <alexgithub@gmail.com>
2021-01-13 21:13:11 +0300

    part of homework 6.1

---
+++ homework_5/src/main/java/com/abra/homework_5/contentprovider/WorkInfoContentProvider.kt
@@ -0,0 +1,56 @@
+package com.abra.homework_5.contentprovider
+
+import android.content.ContentProvider
+import android.content.ContentValues
+import android.content.UriMatcher
+import android.database.Cursor
+import android.net.Uri
+import com.abra.homework_5.database.DataBaseCarInfo
+
+/**
+ * Класс нужен для доступа ко всем работам из этого модуля.
+ * Используется в модуле с домашней 6.1
+ * */
+class WorkInfoContentProvider : ContentProvider() {
+    private var database: DataBaseCarInfo? = null
+
+    companion object {
+        private const val AUTHORITY = "com.abra.homework_5.contentprovider.WorkInfoContentProvider"
+        private const val URI_USER_CODE = 1
+        private val uriMatcher: UriMatcher = UriMatcher(UriMatcher.NO_MATCH).apply {
+            addURI(AUTHORITY, "works_info", URI_USER_CODE)
+        }
+    }
+
+    override fun onCreate(): Boolean {
+        context?.run {
+            database = DataBaseCarInfo.getDataBase(this.applicationContext)
+        }
+        return false
+    }
+
+    override fun query(p0: Uri, p1: Array<out String>?, p2: String?, p3: Array<out String>?, p4: String?): Cursor? {
+        when (uriMatcher.match(p0)) {
+            URI_USER_CODE -> {
+                return database?.getWorkInfoDAO()?.selectAll()
+            }
+        }
+        return null
+    }
+
+    override fun getType(p0: Uri): String? {
+        return "object/*"
+    }
+
+    override fun insert(p0: Uri, p1: ContentValues?): Uri? {
+        return null
+    }
+
+    override fun delete(p0: Uri, p1: String?, p2: Array<out String>?): Int {
+        return 0
+    }
+
+    override fun update(p0: Uri, p1: ContentValues?, p2: String?, p3: Array<out String>?): Int {
+        return 0
+    }
+}
