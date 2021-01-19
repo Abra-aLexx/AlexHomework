1f005933314b5b90dfc5202086a359ac2b390b58
AlexAbra <alexgithub@gmail.com>
2021-01-19 21:16:10 +0300

    homework 7(RxJava)

---
+++ homework_7_rx_java/src/main/java/com/abra/homework_7_rx_java/repositories/DatabaseRepository.kt
@@ -0,0 +1,73 @@
+package com.abra.homework_7_rx_java.repositories
+
+import android.content.Context
+import com.abra.homework_7_rx_java.data.CarInfo
+import com.abra.homework_7_rx_java.data.WorkInfo
+import com.abra.homework_7_rx_java.database.DataBaseCarInfo
+import io.reactivex.rxjava3.core.Single
+import io.reactivex.rxjava3.schedulers.Schedulers
+
+class DatabaseRepository {
+    companion object {
+        private lateinit var database: DataBaseCarInfo
+        fun initDatabase(context: Context) {
+            database = DataBaseCarInfo.getDataBase(context)
+        }
+    }
+
+    fun addWork(info: WorkInfo) {
+        Single.create<WorkInfo> {
+            database.getWorkInfoDAO().addWork(info)
+            it.onSuccess(info)
+        }.subscribeOn(Schedulers.io())
+                .blockingSubscribe()
+    }
+
+    fun updateWorkInfo(info: WorkInfo) {
+        Single.create<WorkInfo> {
+            database.getWorkInfoDAO().update(info)
+            it.onSuccess(info)
+        }.subscribeOn(Schedulers.io())
+                .blockingSubscribe()
+    }
+
+    fun deleteWork(info: WorkInfo) {
+        Single.create<WorkInfo> {
+            database.getWorkInfoDAO().delete(info)
+            it.onSuccess(info)
+        }.subscribeOn(Schedulers.io())
+                .blockingSubscribe()
+    }
+
+    fun getAllWorkListForCar(id: Long): List<WorkInfo> {
+        return Single.create<List<WorkInfo>> {
+            val list = database.getWorkInfoDAO().getAllWorksForCar(id)
+            it.onSuccess(list)
+        }.subscribeOn(Schedulers.io())
+                .blockingGet()
+    }
+
+    fun addCar(info: CarInfo) {
+        Single.create<CarInfo> {
+            database.getCarInfoDAO().add(info)
+            it.onSuccess(info)
+        }.subscribeOn(Schedulers.io())
+                .blockingSubscribe()
+    }
+
+    fun updateCarInfo(info: CarInfo) {
+        Single.create<CarInfo> {
+            database.getCarInfoDAO().update(info)
+            it.onSuccess(info)
+        }.subscribeOn(Schedulers.io())
+                .blockingSubscribe()
+    }
+
+    fun getAllList(): List<CarInfo> {
+        return Single.create<List<CarInfo>> {
+            val list = database.getCarInfoDAO().getAll()
+            it.onSuccess(list)
+        }.subscribeOn(Schedulers.io())
+                .blockingGet()
+    }
+}
